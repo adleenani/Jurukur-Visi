e1addf5539272376521c9995abace1aa38978daa
Adlina Hanani <adleenahanani.azha@gmail.com>
2025-06-02 10:45:20 +0800

    update admin dashboard

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -46,7 +46,7 @@ include 'templates/header_admin_dashboard.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JURUKUR VISI</title>
+    <title>JURUKUR VISI | adlina hanani</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
